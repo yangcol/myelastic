be715e276aeed435e84dd60dcb8fd17b3b50404c
yangqing <qing.yang@dewmobile.net>
2014-06-25 17:46:48 +0800

    Move client code into client directory, but unit test is not changed

---
+++ elasticsearch/src/main/java/com/app/jest/es/client/AbstractSearchResult.java
@@ -0,0 +1,11 @@
+/**
+ * @file AbstractSearchResult.java
+ * @author YangQing
+ * @date 2014/5/14
+ * @brief Abstract class for search result
+ */
+package com.app.jest.es.client;
+
+public abstract class AbstractSearchResult {
+
+}
---
+++ elasticsearch/src/main/java/com/app/jest/es/client/ESBasicSearch.java
@@ -0,0 +1,28 @@
+/**
+ * @file ESBasicSearch.java
+ * @author YangQing
+ * @date 2014/5/14
+ * @brief Interface for search result
+ */
+package com.app.jest.es.client;
+
+import io.searchbox.client.JestClient;
+
+import java.util.List;
+
+public interface ESBasicSearch {
+	public List<? extends AbstractSearchResult> search(JestClient jc,
+			String text, int offset, int limit);
+	
+	public List<? extends AbstractSearchResult> search(JestClient jc,
+			String text);
+	
+	/**
+	 * Interface for get an resource or user
+	 * @param Jest client
+	 * @param id Id to get
+	 * @return <code>AbstractSearchResult</code>
+	 */
+	public AbstractSearchResult get(JestClient jc,
+			String id);
+}
